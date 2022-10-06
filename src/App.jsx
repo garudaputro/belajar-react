4b62fc0f904f04d2adf50cb2562e73c7816c79dd
Iko putro utomo <garudaputro@gmail.com>
2022-10-06 22:11:43 +0000

    belajar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { IconBrandFacebook, IconBrandGoogle } from "@tabler/icons";
 
-function App() {
-    const [count, setCount] = useState(0);
-
+const App = () => {
     return (
-        <div className='App'>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src='/vite.svg' className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://reactjs.org' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
+        <div className='bg-slate-600 grid place-content-center min-h-screen '>
+            <div className="flex gap-x-2">
+
+            <Button>
+                <IconBrandFacebook/>
+                    register
+                
+            </Button>
+            <Button>
+                    <IconBrandGoogle/>
+                    login with google
+            </Button> 
+                       
             </div>
-            <h1>belajar react</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
+          
         </div>
     );
+};
+
+
+
+const Button = (props) => {
+    return (
+        <button className={"[&>svg]:w-5 [&>svg]h-5 [&>svg]stroke-5 bg-blue-600 items-center gap-x-2 flex hover:bg-blue-500 transition text-white px-4 py-2 rounded"}>
+                {props.text || props.children}
+           </button>
+    )
 }
+
+
+
 
 export default App;
