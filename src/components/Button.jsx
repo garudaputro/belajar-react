0fc62a8ed9c4be13547970ca9c6a922f6e029e1b
Iko putro utomo <garudaputro@gmail.com>
2022-10-21 09:14:05 +0000

    penambahan fitur login

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,15 +3,17 @@ import clsx from 'clsx';
 const Button = (props) => {
     const { className = 'bg-blue-600', children, text } = props;
     return (
-        <button
-            {...props}
-            className={clsx(
-                className,
-                '[&>svg]:h-5 [&>svg]:w-5 [&>svg]:stroke-2  flex items-center gap-x-2 text-white px-4 py-2 rounded-md'
-            )}
-        >
-            {text || children}
-        </button>
+        // <button
+        //     {...props}
+        //     className={clsx(
+        //         className,
+        //         '[&>svg]:h-5 [&>svg]:w-5 [&>svg]:stroke-2  flex items-center gap-x-2 text-white px-4 py-2 rounded-md'
+        //     )}
+        // >
+        //     {text || children}
+        // </button>
+    
+<button className="btn btn-primary">Button</button>
     );
 };
 
