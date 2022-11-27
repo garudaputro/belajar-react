6d6fe896e619b3c2f66a2a48c511af9c8667bc99
Iko putro utomo <garudaputro@gmail.com>
2022-11-27 09:45:08 +0000

    added category camplate

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -9,11 +9,12 @@ const Cuisine = () => {
     const [cuisine, setCuisine] = useState([]);
     let params= useParams();
     
+    // mendapatkan data dari api
     const getCuisine = async (name) => {
         const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=4bca4908d81d40ceaef4225b97503a11&cuisine=${name}`);
         const recipes = await data.json();
         
-        setCuisine(recipes.result)
+        setCuisine(recipes.results)
     }
 
     useEffect(() => {
@@ -22,7 +23,22 @@ const Cuisine = () => {
     },[params.type])
 
     return <div>
-       
+        {cuisine.map((item) => {
+            return (
+                <div className='flex gap-5 justify-center mt-5'>
+                    <div key={item.id} className='card card-compact gap-5 space-x-5 w-96   bg-base-100 shadow-xl'>
+                    <figure>
+                        <img src={item.image} alt='Shoes' className='w-full' />
+                    </figure>
+                    <div className='card-body'>
+                        <h2 className='card-title'>
+                            <p>{item.title}</p>
+                        </h2>
+                    </div>
+                </div>
+                </div>
+            )
+        })}
     </div>;
 };
 
